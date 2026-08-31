6750bb1a7a5fa29d4345b8db00d6b56f6ccd6149
Grant <Grantmac12345678911@outlook.com>
2026-08-30 20:27:06 -0400

    Update Project_Grant_McNamara.java
    Added code for demo class

--- Project_Grant_McNamara.java
+++ Project_Grant_McNamara.java
@@ -1,3 +1,49 @@
+import java.util.Scanner;
+
 public class Project_Grant_McNamara {
-    
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter the Policy Number: ");
+        String policyNumber = keyboard.nextLine();
+
+        System.out.print("Please enter the Provider Name: ");
+        String providerName = keyboard.nextLine();
+
+        System.out.print("Please enter the Policyholder’s First Name: ");
+        String firstName = keyboard.nextLine();
+
+        System.out.print("Please enter the Policyholder’s Last Name: ");
+        String lastName = keyboard.nextLine();
+
+        System.out.print("Please enter the Policyholder’s Age: ");
+        int age = keyboard.nextInt();
+        keyboard.nextLine(); 
+
+        System.out.print("Please enter the Policyholder’s Smoking Status (smoker/non-smoker): ");
+        String smokingStatus = keyboard.nextLine();
+
+        System.out.print("Please enter the Policyholder’s Height (in inches): ");
+        double height = keyboard.nextDouble();
+
+        System.out.print("Please enter the Policyholder’s Weight (in pounds): ");
+        double weight = keyboard.nextDouble();
+
+        Policy policy = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
+
+        System.out.println(); 
+
+        System.out.println("Policy Number: " + policy.getPolicyNumber());
+        System.out.println("Provider Name: " + policy.getProviderName());
+        System.out.println("Policyholder’s First Name: " + policy.getPolicyholderFirstName());
+        System.out.println("Policyholder’s Last Name: " + policy.getPolicyholderLastName());
+        System.out.println("Policyholder’s Age: " + policy.getPolicyholderAge());
+        System.out.println("Policyholder’s Smoking Status: " + policy.getSmokingStatus());
+        System.out.printf("Policyholder’s Height: %.1f inches\n", policy.getHeight());
+        System.out.printf("Policyholder’s Weight: %.1f pounds\n", policy.getWeight());
+        System.out.printf("Policyholder’s BMI: %.2f\n", policy.getBMI());
+        System.out.printf("Policy Price: $%.2f\n", policy.getPrice());
+
+        keyboard.close();
+    }
 }
